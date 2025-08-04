3c527fb08b9988369617130437299855bef4aee4
masonwabe <53957610+mxsonwabe@users.noreply.github.com>
2025-08-04 18:43:27 +0200

    add CLI help/Change CLI module

--- main.py
+++ main.py
@@ -1,44 +1,62 @@
 import os
 import sys
+import argparse
 from google import genai
 from dotenv import load_dotenv
 from google.genai import types
-from types import GenerateContentResponse
 
-# get system arguments
-ARGS = sys.argv
+parser = argparse.ArgumentParser(
+    description = "CLI code agent"
+)
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
-    user_prompt = getUserPrompt()
+    setUserPrompt()
+    setFlags()
+
+    args = parser.parse_args()
     # store message history
-    messages = [
-       types.Content(role="user", parts=[types.Part(text=user_prompt)]), 
-    ]
-    response = client.models.generate_content(
-        model= "gemini-2.0-flash-001",
-        contents= messages
+    if args.prompt:
+        messages = [
+           types.Content(
+                role="user",
+                parts=[types.Part(text=args.prompt)]
+            ), 
+        ]
+        response = client.models.generate_content(
+            model= "gemini-2.0-flash-001",
+            contents= messages
+        )
+        displayResponse(args.verbose, args.prompt, response)
+
+def setUserPrompt():
+    parser.add_argument(
+        "prompt",
+        type = str,
+        help = "message to pass to model"
     )
-    displayResponse(user_prompt,response)
 
-def getUserPrompt():
-    if (len(ARGS) < 2):
-        print("No argument supplied.")
-        print("Exiting...")
-        sys.exit(1)
-    str = ""
-    for i in range(1, len(ARGS)):
-        str += ARGS[i]
-    return str
+def setFlags():
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help = "show more info",
+        action = "store_true"
+    )
 
-def displayResponse(user_prompt: str, r: GenerateContentResponse):
-    print("Prompt tokens:", r.usage_metadata.prompt_token_count)
-    print(f"Response tokens: {r.usage_metadata.candidates_token_count}.")
-    print(f"user: {user_prompt}")
-    print(f"model: {r.text}")
+def displayResponse(verbose: bool, prompt: str, r):
+    if verbose:
+        print("Prompt tokens:", r.usage_metadata.prompt_token_count)
+        print(f"Response tokens: {r.usage_metadata.candidates_token_count}.")
+        print(f"user: {prompt}")
+        print(f"model: {r.text}")
+    else:
+        print(f"user: {prompt}")
+        print(f"model: {r.text}")
+
 
 if __name__ == "__main__":
     main()
