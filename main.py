093107824c158df3f58aa7df357ff67e1c3a5738
masonwabe <53957610+mxsonwabe@users.noreply.github.com>
2025-08-04 16:36:15 +0200

    add display

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import sys
 from google import genai
 from dotenv import load_dotenv
+from google.genai import types
+from types import GenerateContentResponse
 
 # get system arguments
 ARGS = sys.argv
@@ -10,16 +12,19 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    
+    user_prompt = getUserPrompt()
+    # store message history
+    messages = [
+       types.Content(role="user", parts=[types.Part(text=user_prompt)]), 
+    ]
     response = client.models.generate_content(
         model= "gemini-2.0-flash-001",
-        contents= getArgs()
+        contents= messages
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}.")
-    print("Response:")
-    print(response.text)
+    displayResponse(user_prompt,response)
 
-def getArgs():
+def getUserPrompt():
     if (len(ARGS) < 2):
         print("No argument supplied.")
         print("Exiting...")
@@ -28,6 +33,12 @@ def getArgs():
     for i in range(1, len(ARGS)):
         str += ARGS[i]
     return str
+
+def displayResponse(user_prompt: str, r: GenerateContentResponse):
+    print("Prompt tokens:", r.usage_metadata.prompt_token_count)
+    print(f"Response tokens: {r.usage_metadata.candidates_token_count}.")
+    print(f"user: {user_prompt}")
+    print(f"model: {r.text}")
 
 if __name__ == "__main__":
     main()
